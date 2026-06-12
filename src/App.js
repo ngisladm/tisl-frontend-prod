ddbdf9325f272f9f4fead204c79d9ae5c41cb2bd
andre <andre.bastos.morais@gmail.com>
2026-06-11 22:28:48 -0300

    aponta api para render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://sl-ti-api.onrender.com";
 
 // ── Responsive hook ──────────────────────────────────────────
 function useIsMobile(){ 
